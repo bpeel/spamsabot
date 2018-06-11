5eeca8374207a8e1fde6ee5dd06662b0b95934b5
Neil Roberts <bpeeluk@yahoo.co.uk>
2018-06-11 09:18:16 +0200

    Add another channel

--- spamsabot.py
+++ spamsabot.py
@@ -30,7 +30,7 @@ conf_dir = os.path.expanduser("~/.spamsabot")
 update_id_file = os.path.join(conf_dir, "update_id")
 apikey_file = os.path.join(conf_dir, "apikey")
 
-banned_users = ['SexGirlsAnalMature', 'LiveSexOnline']
+banned_users = ['SexGirlsAnalMature', 'LiveSexOnline', 'girlsprodating']
 
 with open(apikey_file, 'r', encoding='utf-8') as f:
     apikey = f.read().rstrip()
